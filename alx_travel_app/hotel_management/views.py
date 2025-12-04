994d4cac0585d7c1233e8e34b3949528324f12ba
Martin-bot-man <martinwere14@gmail.com>
2025-12-04 06:47:31 +0300

    feat(hotel_management)added rest_framework and hotel_management to installed_apps

--- alx_travel_app/hotel_management/views.py
+++ alx_travel_app/hotel_management/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Listing
+from rest_framework .decorators import action
+from rest_framework.response import Response
+from .serializers import ListingSerializer
+from rest_framework import viewsets, permissions, status
+
+
 
 # Create your views here.
